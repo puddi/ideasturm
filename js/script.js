38b34474d5a7915bb2ee50d0558744886f6837d9
Staffan Hellman <puddizzle@gmail.com>
2014-10-18 01:42:14 -0700

    basic swapping pages functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
+$(document).ready(function() {
+	if ($.cookie('loginID') == undefined) {
+		$('#ulNav li').toggleClass('noShow')
+	}
+	$('#browseButton').click(loadBrowse);
+	$('#headNav h1 a').click(loadSubmit);
+	/* $('#content').append($('<div id="blurb">').append(
+			$('<p>').text("Ideas are awesome. Share yours with the world.")
+		)
+	); */
+});
+
+function loadSubmit() {
+	$('#content').fadeOut(300, function() {
+		$('#content').empty();
+		$('#content').append($('<div>').attr('id', 'blurb').html('<p>Ideas are awesome. Share yours with the world.</p><div id="mainIdeaField" contenteditable="true"></div><p><a href="#"><span id="mainSubmitButton">Submit</span></a></p>')
+		).fadeIn(300);
+	});
+	
+}
+
+function loadBrowse() {
+	$('#content').fadeOut(300, function() {
+		$('#content').empty();
+		// request for browse stuff here
+		// for now i just have temp data
+		$results = $('<div>').css('text-align', 'center');
+		for (var i = 0; i < 5; ++i) {
+			$results.append(createBox(i, i, i, i, i));
+		}
+		$('#content').append($results).fadeIn(300);
+	});
+}
+
 var $ideaTemplate = $('<div>').addClass('ideaBoxTemplate')
 	.append($('<p>').addClass('title'))
 	.append($('<p>').addClass('author').html('by: <span></span>'))
 	.append($('<hr>'))
 	.append($('<p>').addClass('stats').html('<a href="#"><i class="fa fa-star-o"></i><span class="favorites">test</span></a> <i class="fa fa-comments"></i><span class="comments">test</span> <i class="fa fa-bolt"></i><span class="implementations">test</span>'));
 
+var $
 function createBox(title, author, fav, com, imp) {
 	var $temp = $ideaTemplate.clone();
 	$temp.find('.title').text(title);
@@ -13,18 +48,3 @@ function createBox(title, author, fav, com, imp) {
 	$temp.find('.implementations').text(imp);
 	return $temp;
 }
-
-$(document).ready(function() {
-	$('#content').css({
-		'background-image': 'url(assets/home.jpg)',
-		'background-size': 'cover',
-	});
-	$('#blurb').append($('<div id="yadayada">').css('text-align', 'center'));
-	for (var i = 1; i < 5; i++) {
-		$('#yadayada').append(createBox("hello", "author", "543534", "43", "99"));	
-	}
-	/* $('#content').append($('<div id="blurb">').append(
-			$('<p>').text("Ideas are awesome. Share yours with the world.")
-		)
-	); */
-})
